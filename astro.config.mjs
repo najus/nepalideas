bcff14d1f5736b9db2fecb6e9ca1325d27c07cf7
Sujan <najussujan@gmail.com>
2024-12-29 12:45:34 -0800

    update main URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import AutoImport from "astro-auto-import";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://sujanthapa.com/nepalideas/",
+  site: "https://nepalideas.com",
   integrations: [
     // example auto import component into blog post mdx files
     AutoImport({
